952d68fb09bf9683c2b0f44beff282de81b11c08
TEN ALEXANDER <sasha1keshten@gmail.com>
2025-02-08 20:06:56 +0900

    i  made the   model

--- myproject/rag/experement.py
+++ myproject/rag/experement.py
@@ -1,47 +1,106 @@
-
+import numpy as np 
 import pandas as pd
-from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
-url = "https://raw.githubusercontent.com/sashaTen/investment_app/refs/heads/main/invest/S%26P500data/clusters_df.csv"
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
-selected_columns = [
-        "Ticker", "Returns", "Volatility", "MarketCap", 
-        "ProfitMargins", "DebtToEquity", "ReturnOnEquity", "sharpe_ratio"
-    ]
+df = pd.read_csv(r'C:\Users\HP\Desktop\portfolio_app\data\Data.csv',encoding='ISO-8859-1')
+df.dropna(inplace=True)
+df_copy = df.copy()
+df_copy.reset_index(inplace=True)
+train = df_copy[df_copy['Date'] < '20150101']
+test = df_copy[df_copy['Date'] > '20141231']
+y_train = train['Label']
+train = train.iloc[:, 3:28]
+y_test = test['Label']
+test = test.iloc[:, 3:28]
 
-def load_df(url):
-    df = pd.read_csv(url)
-    return df
+train.replace(to_replace='[^a-zA-Z]', value=' ', regex=True, inplace=True)
+test.replace(to_replace='[^a-zA-Z]', value=' ', regex=True, inplace=True)
 
-def filter_df(df , selected_columns):
-     df_filtered = df[selected_columns]
-     return df_filtered
+new_columns = [str(i) for i in range(0,25)]
+train.columns = new_columns
+test.columns = new_columns
 
+for i in new_columns:
+  train[i] = train[i].str.lower()
+  test[i] = test[i].str.lower()
+  train_headlines = []
+test_headlines = []
 
-def apply_kmeans_clustering(df, n_clusters=3):
-   
-    X = df[['Returns', 'Volatility']]
+for row in range(0, train.shape[0]):
+  train_headlines.append(' '.join(str(x) for x in train.iloc[row, 0:25]))
 
-    # Apply K-Means clustering
-    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
-    df['Cluster'] = kmeans.fit_predict(X)
+for row in range(0, test.shape[0]):
+  test_headlines.append(' '.join(str(x) for x in test.iloc[row, 0:25]))
 
-    # Display the DataFrame with the cluster labels
-    print(df[['Ticker', 'Returns', 'Volatility', 'Cluster']])
+ps = PorterStemmer()
+train_corpus = []
+print('start')
+for i in range(0, len(train_headlines)):
+  
+  # Tokenizing the news-title by words
+  words = train_headlines[i].split()
 
-    # Plotting the clusters
-    plt.figure(figsize=(8, 6))
-    plt.scatter(df['Returns'], df['Volatility'], c=df['Cluster'], cmap='viridis')
-    plt.xlabel('Returns')
-    plt.ylabel('Volatility')
-    plt.title('K-Means Clustering on Returns and Volatility')
-    plt.colorbar(label='Cluster')
-    plt.show()
+  # Removing the stopwords
+  words = [word for word in words if word not in set(stopwords.words('english'))]
 
-    return df
- # Load your filtered data
-df = load_df(url)
-filtered_df = filter_df(df , selected_columns)
-df_clustered = apply_kmeans_clustering(filtered_df, n_clusters=3)
-print(df_clustered.head())
+  # Stemming the words
+  words = [ps.stem(word) for word in words]
+
+  # Joining the stemmed words
+  headline = ' '.join(words)
+
+  # Building a corpus of news-title
+  train_corpus.append(headline)
+
+  test_corpus = []
+
+for i in range(0, len(test_headlines)):
+  
+  # Tokenizing the news-title by words
+  words = test_headlines[i].split()
+
+  # Removing the stopwords
+  words = [word for word in words if word not in set(stopwords.words('english'))]
+
+  # Stemming the words
+  words = [ps.stem(word) for word in words]
+
+  # Joining the stemmed words
+  headline = ' '.join(words)
+
+  # Building a corpus of news-title
+  test_corpus.append(headline)
+
+  train_corpus[0:10]
+  down_words = []
+for i in list(y_train[y_train==0].index):
+  down_words.append(train_corpus[i])
+
+up_words = []
+for i in list(y_train[y_train==1].index):
+  up_words.append(train_corpus[i])
+
+cv = CountVectorizer(max_features=10000, ngram_range=(2,2))
+X_train = cv.fit_transform(train_corpus).toarray()
+X_test = cv.transform(test_corpus).toarray()
+
+lr_classifier = LogisticRegression()
+lr_classifier.fit(X_train, y_train)
+
+lr_y_pred = lr_classifier.predict(X_test)
+
+score1 = accuracy_score(y_test, lr_y_pred)
+score2 = precision_score(y_test, lr_y_pred)
+score3 = recall_score(y_test, lr_y_pred)
+print("---- Scores ----")
+print("Accuracy score is: {}%".format(round(score1*100,2)))
+print("Precision score is: {}".format(round(score2,2)))
+print("Recall score is: {}".format(round(score3,2)))
+
 
--- myproject/rag/views.py
+++ myproject/rag/views.py
@@ -10,8 +10,6 @@ from zenml.client import Client
 def hello(request):
    # simple_ml_pipeline()
 
-   artifact = Client().get_artifact_version("44a244a1-9955-43b3-bc19-45dd3a4a2342")
-   data = artifact.load()
-   return HttpResponse(data)
+   return HttpResponse('helloi ')
 
 
